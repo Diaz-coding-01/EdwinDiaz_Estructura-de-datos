1d2876d59e1e38d9b850f03bfaf177f740cefcdb
Diaz-coding-01 <ed.diaz.hz@gmail.com>
2025-05-31 17:19:58 -0600

    Prueba de push

--- Ejercicio4C_EstructurasSelectivas/src/main/java/EdwinDiaz_20220281/Main.java
+++ Ejercicio4C_EstructurasSelectivas/src/main/java/EdwinDiaz_20220281/Main.java
@@ -10,12 +10,16 @@ public class Main {
             //Guardamos la opc del usuario en la variable grade
             int grade = sc.nextInt();
             sc.close(); //Limpiamos el buffer de scanner
-
-            //Si es mayor a 6 su nota aprueba, si es igual o menor desaprueba
-            if(grade > 6){
-                System.out.println("Aprobado");
-            }else{
-                System.out.println("Desaprobado");
+            if (grade < 0 || grade > 10){
+                System.out.println("La nota ingresada está fuera de los alcances, ingrese una nota válida");
+            }
+            else{
+                //Si es mayor a 6 su nota aprueba, si es igual o menor desaprueba
+                if(grade > 6){
+                    System.out.println("Aprobado");
+                }else{
+                    System.out.println("Desaprobado");
+                }
             }
         } catch (Exception e) {
             System.out.println("Ingrese una nota válida");
